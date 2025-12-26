63bb106496ba12308d4548895b2ef1eb8d1064c3
An-Jie Yu <anjieyu9@gmail.com>
2025-12-25 17:31:38 -0800

    add api functions

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import random
+app = FastAPI()
+random_number = random.randint(1, 10)
+@app.get("/guess/{number}")
+def guess_number(number: int):
+    if number > random_number:
+        return {"result": "too high", 
+                "message": "Your guess was too high. Try again."}
+    elif number < random_number:
+        return {"result": "too low", 
+                "message": "Your guess was too low. Try again."}
+    else:
+        return {"result": "correct",
+                "message": "You got it!"}
+    
+@app.get("/new-game")
+def new_game():
+    global random_number 
+    random_number = random.randint(1, 10)
+    return {"message": "new game started"}
+
+@app.get("/")
+def root():
+    return {"message": "number guessing game root"}
