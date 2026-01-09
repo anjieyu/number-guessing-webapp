8eded0325c15fdf0cc4fa9ce1c932a33871eb0dd
An-Jie Yu <anjieyu9@gmail.com>
2026-01-08 17:22:58 -0800

    create layouts folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,9 @@
-import {createSignal} from 'solid-js';
 import {render} from 'solid-js/web';
+import { NumberGame } from './layouts/NumberGame';
 function App() {
-    const [guess, setGuess] = createSignal("");
-    const [message, setMessage] = createSignal("try to guess a random number from 1 to 10");
-    const handleGuess = async () => {
-        const number = parseInt(guess());
-        if (isNaN(number)) {
-            setMessage("Please enter a valid number.");
-            return;
-        }
-        try {
-            const response = await fetch(`http://localhost:8000/guess/${number}`);
-            const data = await response.json();
-            setMessage(data.message);
-            setGuess("");
-        } catch (error) {
-            setMessage("error connecting to server");
-        }
-    };
-    const handleNewGame = async() => {
-        try {
-            const response = await fetch(`http://localhost:8000/new-game`);
-            const data = await response.json();
-            setMessage(data.message);
-            setGuess("");
-        } catch (error) {
-            setMessage("error connecting to server");
-        }
-    };
     return (
         <div>
-            <h1> number guessing game </h1>
-            <p> {message()} </p>
-            <input
-                type = "number"
-                min = "1"
-                max = "10"
-                value = {guess()}
-                onInput = {(e) => setGuess(e.target.value)}
-                onKeyPress = {(e) => e.key === "Enter" && handleGuess()}
-                placeholder = "1-10"></input>
-            <button
-                onClick = {handleGuess}> Guess </button>
-            <button
-                onClick = {handleNewGame}> New Game </button>
+            <NumberGame/>
         </div>
     );
 }
